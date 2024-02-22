2e0bebc9f0a0fb40c0a1d04167ecd86df1eca97a
Blueberry <98882175+blueberry1001@users.noreply.github.com>
2024-02-22 20:07:24 +0900

    Update Graph.cpp
    二部グラフ判定を追加

--- code/Graph.cpp
+++ code/Graph.cpp
@@ -13,9 +13,11 @@ struct Graph
 	void add_edge(int u, int v, long long w = 1, int dir = 0)
 	{
 		assert(u < V && v < V);
-		if (w != 1)weighted = true;
+		if (w != 1)
+			weighted = true;
 		g[u].push_back({v, w});
-		if (!dir)g[v].push_back({u, w});
+		if (!dir)
+			g[v].push_back({u, w});
 	}
 	void init(int m, int mode = 0, int dir = 0)
 	{
@@ -23,24 +25,27 @@ struct Graph
 		{
 			int u, v;
 			cin >> u >> v;
-			u--,v--;
+			u--, v--;
 			long long w = 1;
-			if (mode)cin >> w;
+			if (mode)
+				cin >> w;
 			add_edge(u, v, w, dir);
 		}
 	}
 	long long distance(int start, int end)
 	{
-		dist.assign(V, ((1LL<<62)-(1LL<<31)));
-		dist[start]=0;
+		dist.assign(V, ((1LL << 62) - (1LL << 31)));
+		dist[start] = 0;
 		if (weighted)
 		{
 			priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
 			pq.push({0, start});
 			while (pq.size())
 			{
-				auto [di, pos] = pq.top();pq.pop();
-				if (dist[pos] < di)continue;
+				auto [di, pos] = pq.top();
+				pq.pop();
+				if (dist[pos] < di)
+					continue;
 				for (auto [to, d] : g[pos])
 				{
 					if (dist[to] > dist[pos] + d)
@@ -57,7 +62,8 @@ struct Graph
 			q.push(start);
 			while (q.size())
 			{
-				int pos = q.front();q.pop();
+				int pos = q.front();
+				q.pop();
 				for (auto [to, d] : g[pos])
 				{
 					if (dist[to] > dist[pos] + 1)
@@ -69,5 +75,37 @@ struct Graph
 			}
 		}
 		return dist[end];
+	}
+	bool isbipartite_graph()
+	{
+		int v = g.size();
+		vector<int> col(v, -1);
+		vector<bool> used(v, false);
+		bool ret = true;
+		for (int i = 0; i < v; i++)
+		{
+			if (used[i])
+				continue;
+			col[i] = 0;
+			[DFS([&](auto &&f, int pos, int pr) -> void
+				 {
+					if(used[pos])return;
+					used[pos]=true;
+					for(auto [to,d]:g[pos])
+					{
+						if(to==pr)continue;
+						if(used[to]&&col[pos]==col[to])
+						{
+							ret = false;
+							return;
+						}
+						if(used[to])continue;
+						col[to]=col[pos]^1;
+						f(f,to,pos);
+					} }),
+			 &i]
+			{ DFS(DFS, i, -1); }();
+		}
+		return ret;
 	}
 };
