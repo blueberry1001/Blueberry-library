51fbfdb6b4c8f399e7c812034289c8e16d779cac
Blueberry <98882175+blueberry1001@users.noreply.github.com>
2024-04-12 11:37:27 +0900

    Create ConvexHulltrick.cpp

---
+++ code/ConvexHulltrick.cpp
@@ -0,0 +1,39 @@
+//https://kopricky.github.io/code/DataStructure_Advanced/convex_hull_trick.html
+template<typename T> class CHT
+{
+private:
+    using ptt = pair<T, T>;
+    bool check(ptt l1, ptt l2, ptt l3){
+        return (l2.first-l1.first)*(l3.second-l2.second)>=(l2.second-l1.second)*(l3.first-l2.first);
+    }
+    T f(int i, T x){
+        return lines[i].first * x + lines[i].second;
+    }
+public:
+    vector<ptt> lines;
+    int head;
+    CHT(): head(0){};
+    void add(T a, T b){
+        ptt line(a, b);
+        while((int)lines.size() - head >= 2 && check(*(lines.end()-2), lines.back(), line)){
+            lines.pop_back();
+        }
+        lines.emplace_back(line);
+    }
+    T get(T x){
+        while((int)lines.size() - head >= 2 && f(head, x) >= f(head + 1, x)){
+            head++;
+        }
+        return f(head, x);
+        // int low = -1, high = lines.size() - 1;
+        // while (high - low > 1) {
+        // 	int mid = (high + low) / 2;
+        //     if(f(mid, x) >= f(mid+1, x)){
+        //         low = mid;
+        //     }else{
+        //         high = mid;
+        //     }
+        // }
+        // return f(high, x);
+    }
+};
